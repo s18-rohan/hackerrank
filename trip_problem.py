b162e4c03a7f93080d65aa572d85664c24a68add
s18-rohan <73271165+s18-rohan@users.noreply.github.com>
2020-10-28 11:48:29 +0530

    Create trip_problem.py
    created trip problem from cp practice

---
+++ trip_problem.py
@@ -0,0 +1,25 @@
+def go():
+    print("invalid")
+'''
+n=int(input("Enter a range"))
+
+for i in range(0,n):
+    r=(float(input("")))
+    #if r==0:
+    #   go()
+    #   break
+    l.append(r)
+    avg=avg+l[i]
+avg=avg/n
+print("average = ",avg)
+
+for i in l:
+    if i< avg:
+        ex1=ex1+(avg-i)
+    else:
+        ex2=ex2+(i-avg)
+if ex1<ex2:
+    print(ex1)
+else:
+    print(ex2)
+
